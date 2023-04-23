e5d8b998bcc2f2868d104597ca14a723a082c884
Spaceforce <71380821+spaceforce-dev@users.noreply.github.com>
2023-04-24 00:19:09 +1000

    got tokens

--- parser.js
+++ parser.js
@@ -1,7 +1,7 @@
 // Regular expression pattern to match Ethereum address and name
 const pattern = /address\/(0x[a-fA-F0-9]{40}).*?>(.+?)<\/td><td class="sorting_1">(.+?)<\/td>/;
 const addresses = require("./raw_data/addresses.js")
-
+const tokens = require("./raw_data/500cryptos")
 
 /**
  * Extracts the Ethereum address and name from a table row using a regular expression
@@ -22,14 +22,25 @@ function extractEthereumAddressAndNameFromTableRow(row) {
     };
 }
 
-Object.values(addresses).forEach(exchange => {
+// Object.values(addresses).forEach(exchange => {
 
-    const rows = exchange.split('<tr');
+//     const rows = exchange.split('<tr');
 
-    rows.forEach(row => {
-        const ethereumAddressAndName = extractEthereumAddressAndNameFromTableRow(row);
-        console.log(ethereumAddressAndName); // { name: 'Binance', address: '0x3f5ce5fbfe3e9af3971dd833d26ba9b5c936f0be' }
-    });
+//     rows.forEach(row => {
+//         const ethereumAddressAndName = extractEthereumAddressAndNameFromTableRow(row);
+//         console.log(ethereumAddressAndName); // { name: 'Binance', address: '0x3f5ce5fbfe3e9af3971dd833d26ba9b5c936f0be' }
+//     });
     
+// });
+
+console.log(tokens)
+
+tokens.data.forEach(token => {
+    if (token.platform) {
+        if (token.platform.id = 1337) {
+            console.log(token.name)
+            console.log(token.platform.token_address)
+        }
+    }
 });
 
